875160cf6700ad505d81843808fc47f34cdfc113
foggy0400 <wfogg1604@gmail.com>
2017-03-06 19:54:30 +1000

    Improved regioncall.py (condensed code, formatted and commented) Added listsystems.py and pyrat.py Added .gitignore, Project_Default.xml and dictionary file

--- source/regioncall.py
+++ source/regioncall.py
@@ -1,33 +1,41 @@
-#Importing csv library and declaring containers for region info
+# Importing csv library and declaring containers for region info
 import csv
+
 regionIds = []
 regionNames = []
+regions = []
+returnreg = []
 
-#Defining function to load the region IDs into an array
-def loadRegionIds():
+# Function to load the region IDs and names into an array
+def load_regions():
     with open('../resources/mapRegions.csv', newline='') as csvfile:
         idlist = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in idlist:
             ids = row[0]
             regionIds.append(ids)
-        return regionIds
-
-#Defining function to load the region names into an array
-def loadRegionNames():
-    with open('../resources/mapRegions.csv', newline='') as csvfile:
-        namelist = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in namelist:
             names = row[1]
             regionNames.append(names)
-        return regionNames
+        regions.append(regionIds)
+        regions.append(regionNames)
+        return regions
 
-#Defining function to query the user for the region they wish to analyse
-def queryRegion(ids, names):
+
+# Function to query the user for the region they wish to analyse
+def query_region():
+    regionlist = load_regions()
+    ids = regionlist[0]
+    names = regionlist[1]
+
     count = 1
-    while (count < (len(ids)-1)):
+    while count < (len(ids) - 1):
         print(repr(count) + ': ' + repr(names[count]))
-        count = count + 1
-    regionSelect = int(input('Select region: '))
-    useRegion = ids[regionSelect]
-    useRegionName = names[regionSelect]
-    print("You selected " + repr(useRegionName) + ", region ID " + repr(useRegion))
+        count += 1
+
+    regionselect = int(input('Select region: '))
+    useregion = ids[regionselect]
+    useregionname = names[regionselect]
+    print("You selected " + repr(useregionname) + ", region ID " + repr(useregion))
+
+    returnreg.append(useregion)
+    returnreg.append(useregionname)
+    return returnreg
